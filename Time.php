c2d98360ec9a1fe2e385dffa68f9d0b1a46e9e7d
Jeroen De Dauw <jeroendedauw@gmail.com>
2013-11-17 20:20:51 +0100

    Update Time.php

--- Time.php
+++ Time.php
@@ -15,7 +15,7 @@ if ( defined( 'DATAVALUES_TIME_VERSION' ) ) {
 	return 1;
 }
 
-define( 'DATAVALUES_TIME_VERSION', '0.1 beta' );
+define( 'DATAVALUES_TIME_VERSION', '0.1' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
