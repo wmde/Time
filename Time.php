0d5d604bf856499dce07aa38abd74200aa661c4d
addshore <addshorewiki@gmail.com>
2014-02-11 13:28:30 +0100

    Bump version to 0.2 and update release notes

--- Time.php
+++ Time.php
@@ -15,7 +15,7 @@ if ( defined( 'DATAVALUES_TIME_VERSION' ) ) {
 	return 1;
 }
 
-define( 'DATAVALUES_TIME_VERSION', '0.1' );
+define( 'DATAVALUES_TIME_VERSION', '0.2' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
