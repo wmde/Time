2db8fb0a494b213ef68f79d848bd371079152d1d
jeroendedauw <jeroendedauw@gmail.com>
2014-10-09 21:00:56 +0200

    Make installable with DV 1.x and bump to 0.6.1

--- Time.php
+++ Time.php
@@ -15,7 +15,7 @@ if ( defined( 'DATAVALUES_TIME_VERSION' ) ) {
 	return 1;
 }
 
-define( 'DATAVALUES_TIME_VERSION', '0.6' );
+define( 'DATAVALUES_TIME_VERSION', '0.6.1' );
 
 if ( defined( 'MEDIAWIKI' ) ) {
 	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
