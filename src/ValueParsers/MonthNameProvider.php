e7bdd94e0c399306c0572133ea0a57829ba9a48f
Thiemo Mättig <thiemo.maettig@wikimedia.de>
2016-02-23 13:02:47 +0100

    Add MonthNameProvider and MonolingualMonthNameProvider

---
+++ src/ValueParsers/MonthNameProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace ValueParsers;
+
+/**
+ * A common interface for both localizing month names in formatters as well as unlocalizing month
+ * names in parsers.
+ *
+ * @since 0.8.4
+ *
+ * @license GPL-2.0+
+ * @author Thiemo Mättig
+ */
+interface MonthNameProvider {
+
+	/**
+	 * @param string $languageCode
+	 *
+	 * @return string[] Array mapping month numbers (1 to 12) to localized month names.
+	 */
+	public function getLocalizedMonthNames( $languageCode );
+
+	/**
+	 * @param string $languageCode
+	 *
+	 * @return int[] Array mapping localized month names (possibly including full month names,
+	 * genitive names and abbreviations) to month numbers (1 to 12).
+	 */
+	public function getMonthNumbers( $languageCode );
+
+}
