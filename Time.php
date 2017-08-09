1c223f4f17f30a3e879076f1930eae724a92a7ed
Kunal Mehta <legoktm@member.fsf.org>
2017-08-09 15:28:13 -0400

    Remove MediaWiki integration

--- Time.php
+++ Time.php
@@ -17,16 +17,3 @@ if ( defined( 'DATAVALUES_TIME_VERSION' ) ) {
 
 define( 'DATAVALUES_TIME_VERSION', '0.8.4' );
 
-if ( defined( 'MEDIAWIKI' ) ) {
-	$GLOBALS['wgExtensionCredits']['datavalues'][] = array(
-		'path' => __DIR__,
-		'name' => 'DataValues Time',
-		'version' => DATAVALUES_TIME_VERSION,
-		'author' => array(
-			'The Wikidata team',
-		),
-		'url' => 'https://github.com/DataValues/Time',
-		'description' => 'Time value objects, parsers and formatters',
-		'license-name' => 'GPL-2.0+'
-	);
-}
